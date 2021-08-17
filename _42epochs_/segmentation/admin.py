b53889dca484f2705ad8369496720e50f01b3f09
Ohlomonchick <64347118+Ohlomonchick@users.noreply.github.com>
2021-08-17 12:56:20 +0300

    admin + form saving

--- _42epochs_/segmentation/admin.py
+++ _42epochs_/segmentation/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+class CTAdmin(admin.ModelAdmin):
+    list_display = ('ct_image',
+                    'segmented_image',
+                    'mask',
+                    'damage',
+                    'time_create',)
+
+
+admin.site.site_title = 'Админка'
+admin.site.site_header = 'Админка сайта'
+
+admin.site.register(CT, CTAdmin)
--- _42epochs_/segmentation/views.py
+++ _42epochs_/segmentation/views.py
@@ -14,9 +14,16 @@ def index(request):
 class AddCT(CreateView):
     form_class = AddCTForm
     template_name = 'segmentation/addpage.html'
-    # success_url = reverse_lazy('home')
+    success_url = reverse_lazy('home')
     raise_exception = True
 
     def form_valid(self, form):
         print(form.cleaned_data)
+        form.save()
+        # TODO вызывать сегментацию изображения
         return redirect('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Загрузка КТ'
+        return context
