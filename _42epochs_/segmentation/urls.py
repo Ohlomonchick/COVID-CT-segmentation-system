0aa1236e2f45cffe3d52b8f5f3aae3b6baac3d45
Ohlomonchick <64347118+Ohlomonchick@users.noreply.github.com>
2021-08-28 11:29:00 +0300

    CT uploading is main page now

--- _42epochs_/segmentation/urls.py
+++ _42epochs_/segmentation/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from segmentation.views import *
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('form/', AddCT.as_view(), name='form'),
-    path('form/result/<slug:ct_pk>/', ShowSegmented.as_view(), name='result')
+    path('', AddCT.as_view(), name='home'),
+    # path('form/', AddCT.as_view(), name='form'),
+    path('result/<slug:ct_pk>/', ShowSegmented.as_view(), name='result')
 ]
